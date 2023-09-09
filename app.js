ec3db8c3e103d36e22e69a37137a0c4cafad6ef3
danibrainy94 <danibrainy94@gmail.com>
2023-09-09 14:37:11 +0100

    master

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//getting the element from the DOM
 const currenTime = document.querySelector('.current-time')
 
 
 const presentTime = new Date;
 
+//function to calculate the and return the UTC time in milliseconds
 function toMilliSeconds (hours, minutes, seconds, milliseconds) {
     let milliSecToHours = hours * 60 * 60 * 1000;
     let milliSecToMinutes = minutes * 60 * 1000;
@@ -12,5 +14,5 @@ function toMilliSeconds (hours, minutes, seconds, milliseconds) {
 }
 
 
-
+//assigning the value of the time calculated to the element in the DOM
 currenTime.textContent = toMilliSeconds(presentTime.getUTCHours(), presentTime.getUTCMinutes(), presentTime.getUTCSeconds(), presentTime.getUTCMilliseconds());
